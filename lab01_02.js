14d9bc8172c068f531dd1b2ca3c6825ca8491095
MinhDuy <98084393+MinhDuy18@users.noreply.github.com>
2023-09-12 17:26:27 +0700

    change name
    commit

--- lab01_02.js
+++ lab01_02.js
@@ -227,20 +227,6 @@ console.log(tips_4);
 console.log(totals_4);
 
 //Coding Challenge #5
-/*
-Coding Challenge #9
-Given an array of forecasted maximum temperatures, the thermometer displays a
-string with the given temperatures. Example: [17, 21, 23] will print "... 17oC in 1
-days ... 21oC in 2 days ... 23oC in 3 days ..."
-Your tasks:
-1. Create a function 'printForecast' which takes in an array 'arr' and logs a
-string like the above to the console. Try it with both test datasets.
-2. Use the problem-solving framework: Understand the problem and break it up
-into sub-problems!
-Test data:
-§ Data 1: [17, 21, 23]
-§ Data 2: [12, 5, -5, 0, 4]
-*/
 console.log("Coding Challenge #5");
 //1
 const printForecast = function (arr) {
@@ -253,7 +239,8 @@ const printForecast = function (arr) {
 //2
 printForecast([17, 21, 23]);
 printForecast([12, 5, -5, 0, 4]);
-    
+
+
 
 
 
